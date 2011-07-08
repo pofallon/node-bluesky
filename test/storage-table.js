aa760213599ef1a779f01c68d8c04dd6eb62d25e
Paul O'Fallon <paul@ofallonfamily.com>
2011-07-08 14:49:28 -0400

    Added more storage functionality, nodeunit tests

--- test/storage-table.js
+++ test/storage-table.js
@@ -1,28 +1,84 @@
-var account = "your_acct_here";
-var key = "your_key_here";
+var fs = require('fs');
+var testCase = require('nodeunit').testCase;
+var storage = require('../lib/azure').storage;
 
-var s = require('../lib/azure').storage;
+module.exports = testCase({
 
-s.listTables(account,key,function(error,tables) {
-  
-  if (error) {
-    console.log("Error: " + error);
-  } else {
-    console.log(JSON.stringify(tables));
-  }
-  
-});
+  setUp: function (callback) {
+    var testCredentials = JSON.parse(fs.readFileSync(process.env.HOME + '/.azurejs/test.json','ascii'));
+    this.account = testCredentials.account;
+    this.key = testCredentials.key;
 
-/* var t = s.table(account,key,"testTable");
+    this.tableName = "foobar";
+    this.partitionKey = "foo";
+    this.rowKey = "bar";
+    callback();
+  },
 
-var rs = t.query();
+  createTable: function (test) {
+    var theTable = this.tableName;
+    storage.createTable(this.account, this.key, theTable, function(err,table) {
+      test.equals(err,null);
+      test.equals(table.name,theTable);
+      test.done();
+    });
+  },
 
-rs.all(function(error, results) {
-  
-  console.log(JSON.stringify(results));
-  
-}); */
+  createdTableInList: function (test) {
+    var theTable = this.tableName;
+    storage.listTables(this.account, this.key, function(err,tables) {
+      test.notStrictEqual(tables.indexOf(theTable),-1);
+      test.done();
+    });
+  },
 
-s.table(account,key,"testTable").query().all(function(error, results) {
-  console.log(JSON.stringify(results));
+  tableInsertRow: function (test) {
+    var t = new storage.table(this.account, this.key, this.tableName);
+    t.insert(this.partitionKey,this.rowKey,{'one':'uno'}, function(err) {
+      test.equals(err,null);
+      test.done();
+    });
+  },
+
+  tableQueryAll: function (test) {
+    var myPartition = this.partitionKey;
+    var myRow = this.rowKey;
+
+    var t = new storage.table(this.account, this.key, this.tableName);
+    t.query().all(function(err, results) {
+      test.equals(results[0].PartitionKey,myPartition);
+      test.equals(results[0].RowKey,myRow);
+      test.equals(results[0].one,'uno');
+      test.done();
+    });
+  },
+
+  tableDeleteRow: function (test) {
+    var t = new storage.table(this.account, this.key, this.tableName);
+    t.del(this.partitionKey,this.rowKey,function(err) {
+      test.equals(err,null);
+      test.done();
+    });
+  },
+
+  removeTable: function (test) {
+    storage.removeTable(this.account, this.key, this.tableName, function(err) {
+      test.equals(err,null);
+      test.done();
+    });
+  },
+
+  tableNoLongerInList: function (test) {
+    var theTable = this.tableName;
+    storage.listTables(this.account, this.key, function(err, tables) {
+      test.strictEqual(tables.indexOf(theTable),-1);
+      test.done();
+    });
+  },
+
+  /* tearDown: function (callback) {
+    // Need a way to remove a table *if* it exists (and not error otherwise)
+    storage.removeTable(this.account, key, this.tableName);
+  } */
+
 });
