ecc1a634dfa88088b95d0af52c249bad5a8c8a5b
Paul O'Fallon <paul@ofallonfamily.com>
2011-09-07 11:13:12 -0400

    jslint/hint & format fixes

--- win-unit.js
+++ win-unit.js
@@ -1,1 +1,10 @@
-var reporter = require('nodeunit').reporters.default;reporter.run(['test'],{  'error_prefix': '',  'error_suffix': '',  'ok_prefix': '',  'ok_suffix': '',  'bold_prefix': '',  'bold_suffix': '',  'assertion_prefix': '',  'assertion_suffix': ''});
+var reporter = require('nodeunit').reporters.default;
+reporter.run(['test'],{
+  'error_prefix': '',
+  'error_suffix': '',
+  'ok_prefix': '',
+  'ok_suffix': '',
+  'bold_prefix': '',
+  'bold_suffix': '',
+  'assertion_prefix': '',
+  'assertion_suffix': ''});
