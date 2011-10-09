a04d38573d6d090e3ad588675866c865b9b9e68d
Paul O'Fallon <paul@ofallonfamily.com>
2011-10-08 20:10:15 -0400

    Stubbed out queue support

---
+++ test/storage-queue.js
@@ -0,0 +1,24 @@
+/*!
+ * node-azure
+ * Copyright(c) 2011 Paul O'Fallon <paul@ofallonfamily.com>
+ * MIT Licensed
+ */
+
+var fs = require('fs');
+var testCase = require('nodeunit').testCase;
+
+var path = process.env.HOME || (process.env.HOMEDRIVE + process.env.HOMEPATH);
+var testCredentials = JSON.parse(fs.readFileSync(path + '/.azurejs/test.json','ascii'));
+    
+var storage = require('../lib/azure').storage({account: testCredentials.account, key: testCredentials.key});
+
+module.exports = testCase({
+
+  setUp: function (callback) {
+
+    this.containerName = "foobar";
+    callback();
+    
+  },
+  
+});
